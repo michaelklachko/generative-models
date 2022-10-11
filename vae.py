655e31bd16442748e37b572a1bca85869866ab98
Michael Klachko <mklachko@d-matrix.ai>
2022-10-10 22:23:03 -0700

    added training code

--- vae.py
+++ vae.py
@@ -19,7 +19,11 @@ def get_args_parser(add_help=True):
     parser = argparse.ArgumentParser(description="PyTorch Detection Training", add_help=add_help)
 
     parser.add_argument("--data_dir", default="data", type=str, help="path to save outputs")
-    parser.add_argument("--calib_size", default=1, type=int)
+    parser.add_argument("--train_batch_size", default=1, type=int)
+    parser.add_argument("--test_batch_size", default=1, type=int)
+    parser.add_argument("--lr", default=0.001, type=float)
+    parser.add_argument("--wd", default=0.0, type=float)
+    parser.add_argument("--beta", default=0.01, type=float)
     parser.add_argument("--evaluate", dest="evaluate", help="run FP32 evaluation as baseline", action="store_true")
     
     return parser
@@ -75,20 +79,12 @@ class Encoder(nn.Module):
         
     def forward(self, x):
         # input x is (bs, 3, 32, 32) for cifar images
-        
         x = self.conv1(x)
-        
         x = self.max_pool(x)
-        
         x = self.conv2(x)
-        
         x = self.max_pool(x)
-        
         x = self.conv3(x)
-        # 
-        # x = self.max_pool(x)
-        
-        
+
         x = x.reshape(x.shape[0], -1)  # should be (bs, 8*8*256)
         
         x = self.fc(x)  # should be (bs, 2*latent_size)
@@ -103,7 +99,7 @@ class Decoder(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=5, stride=1, padding=2)  # TODO should we use smaller kernels here?
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, stride=1, padding=2)
         self.conv3 = nn.Conv2d(in_channels=128, out_channels=3, kernel_size=5, stride=1, padding=2)
-        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+        self.upsample = nn.Upsample(scale_factor=2, mode="bilinear")  # do we want upsample of deconv layer?
         self.init_size = int(np.sqrt(latent_size))
         
     def forward(self, x):
@@ -124,7 +120,6 @@ class VAE(nn.Module):
         super().__init__()
         self.encoder = Encoder(latent_size)
         self.decoder = Decoder(latent_size)
-        self.loss = nn.MSELoss()
         self.latent_size = latent_size
         
     
@@ -135,50 +130,93 @@ class VAE(nn.Module):
         # compute reconstruction_loss between decoded and original images
         # compute gaussian_loss between stats vector and Normal distribution stats (mean=0 and std=1)
 
-        stats = self.encoder(x)   # stats is a vector (latent_size, 2) with (mean, std) rows 
+        self.stats = self.encoder(x)   # stats is a vector (latent_size, 2) with (mean, std) rows 
         
-        means = stats[:, :, 0]
-        stds = stats[:, :, 1]
+        means = self.stats[:, :, 0]
+        stds = self.stats[:, :, 1]
         
         # reparametrization trick: sample a value as mean + std * N(0, 1)
-        N = torch.randn((x.shape[0], self.latent_size))
-        latent_vector = means + stds * N
+        self.normal = torch.randn((x.shape[0], self.latent_size), device='cuda')
+        assert self.normal.shape == means.shape == stds.shape
+        latent_vector = means + stds * self.normal
         
         out = self.decoder(latent_vector)
         
-        return out, stats
+        return out
 
 # %%
 parser = get_args_parser()
 #args = get_args_parser().parse_args()
 args, unknown = parser.parse_known_args()
 
-train_dataloader, test_dataloader = get_data(data_dir=args.data_dir)
-sample = iter(test_dataloader).next()[0].reshape(1, 3, 32, 32)
 
-vae = VAE(latent_size=64)
+train_dataloader, test_dataloader = get_data(
+    data_dir=args.data_dir, 
+    train_batch_size=args.train_batch_size, 
+    test_batch_size=args.test_batch_size
+    )
+
+input_image = iter(test_dataloader).next()[0][0].reshape(1, 3, 32, 32)
+
+latent_size = 64
+
+vae = VAE(latent_size=latent_size).cuda()
  
-out, stats = vae(sample)
+reconstructed_image = vae(input_image.cuda())
 
 # %%
 
-sample = sample.squeeze(0).permute(1,2,0)
-sample = sample - sample.min()
-sample = sample / sample.max()
-sample = sample.cpu().detach()
+def plot_image(image, name='image.png'):
+    image = image.squeeze(0).permute(1,2,0)
+    image = image - image.min()
+    image = image / image.max()
+    image = image.cpu().detach()
 
-plt.imshow(sample)
-plt.show()
-plt.savefig('original_image.png')
+    plt.imshow(image)
+    plt.show()
+    plt.savefig(name)
+    
+plot_image(input_image, 'original_image.png')
+plot_image(reconstructed_image, 'reconstructed_image.png')
 
 # %%
-image = out.squeeze(0).permute(1,2,0)
-image = image - image.min()
-image = image / image.max()
-image = image.cpu().detach()
 
-plt.imshow(image)
-plt.show()
-plt.savefig('reconstructed_image.png')
+# train
+
+# create optimizer
+optim = torch.optim.AdamW(vae.parameters(), lr=args.lr, weight_decay=0.000)
+
+reconstruction_loss = nn.MSELoss()
+beta_loss = nn.KLDivLoss()
+
+normal_stats = torch.zeros((args.train_batch_size, latent_size, 2), device='cuda')
+normal_stats[:, :, 1] = 1
+
+count = 0
+vae.train()
+
+for epoch in range(10):
+    print(f'\n\nEpoch {epoch}\n\n')
+    for image, label in train_dataloader:
+        image = image.cuda()
+        reconstructed = vae(image)
+        
+        rec_loss = reconstruction_loss(image, reconstructed)
+        b_loss = beta_loss(vae.stats, normal_stats)
+        loss = rec_loss + args.beta * b_loss.abs()
+        
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        count += 1
+        
+        if count % 1000 == 0:
+            print(count, rec_loss.item(), b_loss.item())
+            name = f'reconstructed_i{count}_b{args.beta}_bs{args.train_batch_size}_lr{args.lr}_wd{args.wd}.png'
+            vae.eval()
+            reconstructed_image = vae(input_image.cuda())
+            vae.train()
+            plot_image(reconstructed_image, name)
+        
 
 # %%
